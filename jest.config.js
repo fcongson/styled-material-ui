4443e00a524c1f82c8bc2639e93b87baea4da4c7
Frank Congson <fcongson@gmail.com>
2020-02-10 14:56:27 +1300

    Add jest and enzyme

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/enzyme.setup.js'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}
