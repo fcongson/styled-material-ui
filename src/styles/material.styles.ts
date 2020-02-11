638e3f5aba906b9921b8e6c77f215f40f10997fb
Frank Congson <fcongson@gmail.com>
2020-02-11 15:12:14 +1300

    Organize material styles

--- src/styles/material.styles.ts
+++ src/styles/material.styles.ts
@@ -1,72 +1,92 @@
 import { withStyles } from '@material-ui/core'
 import { checkbox, icon, input, select, tag } from './global.styles'
 
-export const MaterialStyles = withStyles({
-  '@global': {
-    '.MuiAutocomplete-inputRoot[class*="MuiInput-root"]': {
+const muiAutoComplete = {
+  '.MuiAutocomplete-inputRoot[class*="MuiInput-root"]': {
+    ...input.size,
+    ...input.spacing,
+    ...input.text,
+    '& .MuiAutocomplete-input:first-child': {
       ...input.size,
       ...input.spacing,
       ...input.text,
-      '& .MuiAutocomplete-input:first-child': {
-        ...input.size,
-        ...input.spacing,
-        ...input.text,
-        paddingRight: 60
-      },
-      '&.multiple': {
-        paddingRight: 64
-      }
+      paddingRight: 60
     },
-    '.MuiInput-underline': {
-      '&:not(.Mui-disabled):before, &:hover:not(.Mui-disabled):before': input.underline.default,
-      '&:after': input.underline.focused
+    '&.multiple': {
+      paddingRight: 64
+    }
+  },
+  '.MuiAutocomplete-paper': select.options.container,
+  '.MuiAutocomplete-listbox': select.options.item.text,
+  '.MuiAutocomplete-option': {
+    '& span.unchecked': {
+      ...checkbox.default,
+      ...checkbox.unchecked
     },
-    '.MuiAutocomplete-paper': select.options.container,
-    '.MuiAutocomplete-listbox': select.options.item.text,
-    '.MuiAutocomplete-option': {
+    '& span.checked': {
+      ...checkbox.default,
+      ...checkbox.checked
+    },
+    '&[data-focus="true"], &[aria-selected="true"]': {
+      ...select.options.item.selected,
       '& span.unchecked': {
-        ...checkbox.default,
-        ...checkbox.unchecked
+        ...checkbox.focused
       },
       '& span.checked': {
-        ...checkbox.default,
-        ...checkbox.checked
-      },
-      '&[data-focus="true"], &[aria-selected="true"]': {
-        ...select.options.item.selected,
-        '& span.unchecked': {
-          ...checkbox.focused
-        },
-        '& span.checked': {
-          ...checkbox.focused
-        }
-      },
-      '&.multiple': {
-        padding: 0,
-        ...select.options.item.multiple,
-        '& .MuiCheckbox-root': {
-          padding: '4px 8px 4px 16px'
-        },
-        '& .MuiCheckbox-colorSecondary.Mui-checked:hover': {
-          ...select.options.item.multiple
-        }
+        ...checkbox.focused
       }
     },
-    '.MuiIconButton-root': {
-      '&:hover': icon.button,
-      '& svg': icon.svg
-    },
-    '.MuiAutocomplete-clearIndicator': icon.button,
-    '.MuiAutocomplete-popupIndicator': {
-      ...icon.button,
-      marginRight: 6
-    },
-    '.MuiTouchRipple-rippleVisible': icon.animation,
-    '.MuiChip-root': {
-      ...tag.root,
-      '& svg': tag.svg
-    },
-    '.MuiChip-label': tag.label,
-    '.MuiChip-deleteIcon': tag.delete
+    '&.multiple': {
+      padding: 0,
+      ...select.options.item.multiple,
+      '& .MuiCheckbox-root': {
+        padding: '4px 8px 4px 16px'
+      },
+      '& .MuiCheckbox-colorSecondary.Mui-checked:hover': {
+        ...select.options.item.multiple
+      }
+    }
+  },
+  '.MuiAutocomplete-clearIndicator': icon.button,
+  '.MuiAutocomplete-popupIndicator': {
+    ...icon.button,
+    marginRight: 6
+  }
+}
+
+const muiInput = {
+  '.MuiInput-underline': {
+    '&:not(.Mui-disabled):before, &:hover:not(.Mui-disabled):before': input.underline.default,
+    '&:after': input.underline.focused
+  }
+}
+
+const muiIcon = {
+  '.MuiIconButton-root': {
+    '&:hover': icon.button,
+    '& svg': icon.svg
+  }
+}
+
+const muiTouchRipple = {
+  '.MuiTouchRipple-rippleVisible': icon.animation
+}
+
+const muiChip = {
+  '.MuiChip-root': {
+    ...tag.root,
+    '& svg': tag.svg
+  },
+  '.MuiChip-label': tag.label,
+  '.MuiChip-deleteIcon': tag.delete
+}
+
+export const MaterialStyles = withStyles({
+  '@global': {
+    ...muiAutoComplete,
+    ...muiInput,
+    ...muiIcon,
+    ...muiTouchRipple,
+    ...muiChip
   }
 })(() => null)
