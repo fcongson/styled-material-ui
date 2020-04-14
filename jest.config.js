4ca2f098f42f0a3a405e1127ccec57ee7f3d7d7b
Frank Congson <fcongson@gmail.com>
2020-04-14 21:46:34 +1200

    Update jest snapshots

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest'
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
 }
